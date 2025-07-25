ee871034fd5b6d72bd7393a625e025533dbeb9fb
Dungslikert <dung1322003@gmail.com>
2025-07-25 21:34:50 +0700

    fix layout

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Hypo from '../ai/components/Hypo';
-import styles from '../ai/styles/Hypo.module.css';
+
+import hypoStyles from '../ai/styles/Hypo.module.css';          
+import layoutStyles from '../styles/components/Layout.module.css'; 
 
 const Layout = () => {
   return (
-    <div className={styles.layout}>
+    <div className={layoutStyles.layout}>
       <Sidebar />
-      <Hypo />
-      <div className={styles.content}>
+      <Hypo className={hypoStyles.hypoContainer} />
+      <div className={layoutStyles.content}>
         <Outlet />
       </div>
     </div>
